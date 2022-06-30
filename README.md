a85e279bd6978aa024bb673ebbff0b709e47932c
rg3915 <regis42santos@gmail.com>
2022-06-30 00:14:17 -0300

    add project

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# django-mysql
+# django-experience
+
+Tutorial Django Experience 2022
+
+## Este projeto foi feito com:
+
+* [Python 3.10.4](https://www.python.org/)
+* [Django 4.0.4](https://www.djangoproject.com/)
+* [Django Rest Framework 3.12.4](https://www.django-rest-framework.org/)
+
+## Como rodar o projeto?
+
+* Clone esse repositório.
+* Crie um virtualenv com Python 3.
+* Ative o virtualenv.
+* Instale as dependências.
+* Rode as migrações.
+
+```
+git clone https://github.com/rg3915/django-mysql.git
+cd django-mysql
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python contrib/env_gen.py
+python manage.py migrate
+python manage.py createsuperuser --username="admin" --email=""
+```
+
---
+++ backend/urls.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('backend.core.urls', namespace='core')),
+    path('', include('backend.crm.urls', namespace='crm')),
+    path('admin/', admin.site.urls),
+]
+
+# djoser
+urlpatterns += [
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.authtoken')),
+]
+
+# swagger
+urlpatterns += [
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # noqa E501
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # noqa E501
+]
